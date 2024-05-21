44cf6454667841cb61980e53d79572e43c0e459d
Alexander <onemem2204@gmail.com>
2024-05-21 16:34:37 +0700

    Improve system login to apps, configured parsing

--- app/src/main/java/com/example/touniversity2/MainActivity.java
+++ app/src/main/java/com/example/touniversity2/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -45,8 +47,12 @@ int progress = 0;
 University content;
 ProgressBar pb;
 Handler handler = new Handler(Looper.getMainLooper());
+    private int step=1;
+
     static final int time_streams = 500;
     UniversityDatabase universityDatabase;
+    private static final String TRACKSTART = "trackstart";
+private boolean hasVisited;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,100 +64,91 @@ Handler handler = new Handler(Looper.getMainLooper());
         pb.setMax(200);
         setProgressValue(progress);
 
+        SharedPreferences sp = getSharedPreferences(TRACKSTART, Context.MODE_PRIVATE);
+        hasVisited = sp.getBoolean("hasVisited", false);
+        if (!hasVisited) {
+            SharedPreferences.Editor e = sp.edit();
+            e.putBoolean("hasVisited", true);
+            e.commit();
+        }
 
 
-        RoomDatabase.Callback myCallback = new RoomDatabase.Callback() {
-            @Override
-            public void onCreate(@NonNull SupportSQLiteDatabase db) {
-                super.onCreate(db);
-            }
-
-            @Override
-            public void onOpen(@NonNull SupportSQLiteDatabase db) {
-                super.onOpen(db);
-            }
-        };
-        universityDatabase = Room.databaseBuilder(getApplicationContext(), UniversityDatabase.class,
-                "University").addCallback(myCallback).build();
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    University MGU1 = new University("МГУ", "Системное программирование и компьютерные науки", 405, 300, "Информатика, Математика (профиль), Русский язык, Физика", "Платное/Бесплатное", "Москва", 409610,"https://vuzopedia.ru/vuz/1/programs/bakispec/62",  4, "Математика");
-                    University MGU2 = new University("МГУ", "Прикладная математика и физика", 275, 126, "Математика (профиль), Русский язык, Физика", "Платное/Бесплатное", "Москва", 435970, "https://vuzopedia.ru/vuz/1/napr/93", 4, "Физика");
-                    University MGTU1 = new University("МГТУ", "Бизнес-информатика", 278, 241, "Математика (профиль), Русский язык, Обществознание", "Платное/Бесплатное" , "Москва",324143 , "https://vuzopedia.ru/vuz/4/programs/bakispec/603", 3, "Без вступительных испытаний");
-                    University MGTU2 = new University("МГТУ", "Бизнес-информатика", 278, 241, "Информатика, Математика (профиль), Русский язык", "Платное/Бесплатное" , "Москва",324143 , "https://vuzopedia.ru/vuz/4/programs/bakispec/603", 3, "Без вступительных испытаний");
+        if(hasVisited){
+            step=5;
 
 
-                    University MFTI1 = new University("МФТИ", "Системный анализ и управление", 295, 280, "Информатика, Математика (профиль), Русский язык", "Платное/Бесплатное", "Москва", 504000, "https://vuzopedia.ru/vuz/591/programs/bakispec/130",  3, "Без вступительных испытаний");
-                    University MFTI2 = new University("МФТИ", "Системный анализ и управление", 295, 280, "Математика (профиль), Русский язык, Физика", "Платное/Бесплатное", "Москва", 504000, "https://vuzopedia.ru/vuz/591/programs/bakispec/130",  3, "Без вступительных испытаний");
-
-                    University NGU1 = new University("НГУ", "Организационная психология", 249, 177, "Биология, Математика (профиль), Русский язык", "Платное/Бесплатное" ,"Новосибирск", 125000, "https://vuzopedia.ru/vuz/1612/programs/bakispec/561", 4, "Без вступительных испытаний");
-
-
-                    addback(MGU1);
-                    addback(MGU2);
-                    addback(MGTU1);
-                    addback(MGTU2);
-
-                    addback(MFTI1);
-                    addback(MFTI2);
-                    addback(NGU1);
-
-
-
-                    addback(getContent("https://vuzopedia.ru/vuz/1848/programs/bakispec/87"));
-                    addback(getContent("https://vuzopedia.ru/vuz/725/programs/bakispec/164"));
-                    addback(getContent("https://vuzopedia.ru/vuz/1189/programs/bakispec/2272"));
-                    Thread.sleep(1000);
-                    addback(getContent("https://vuzopedia.ru/vuz/1567/programs/bakispec/1666"));
-                    addback(getContent("https://vuzopedia.ru/vuz/342/programs/bakispec/108"));
-                    addback(getContent("https://vuzopedia.ru/vuz/342/programs/bakispec/2277"));
-                    Thread.sleep(1000);
-                    addback(getContent("https://vuzopedia.ru/vuz/1189/programs/bakispec/46"));
-                    addback(getContent("https://vuzopedia.ru/vuz/725/programs/bakispec/1546"));
-                    addback(getContent("https://vuzopedia.ru/vuz/342/programs/bakispec/81"));
-                    Thread.sleep(1000);
-                    addback(getContent("https://vuzopedia.ru/vuz/1751/programs/bakispec/186"));
-                    addback(getContent("https://vuzopedia.ru/vuz/1751/programs/bakispec/6122"));
-                    addback(getContent("https://vuzopedia.ru/vuz/2554/programs/bakispec/125"));
-                    addback(getContent("https://vuzopedia.ru/vuz/1751/programs/bakispec/85"));
-                    addback(getContent("https://vuzopedia.ru/vuz/1612/programs/bakispec/400"));
-
-                    //Thread.sleep(500);
-
-
-                } catch (IOException ex) {
-                    content = null;
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+        }
+        else{
+            RoomDatabase.Callback myCallback = new RoomDatabase.Callback() {
+                @Override
+                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                    super.onCreate(db);
                 }
-            }
-        }).start();
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    Thread.sleep(1000);
-//                    addback(getContent("https://vuzopedia.ru/vuz/1189/programs/bakispec/46"));
-//                    addback(getContent("https://vuzopedia.ru/vuz/725/programs/bakispec/1546"));
-//                    addback(getContent("https://vuzopedia.ru/vuz/342/programs/bakispec/81"));
-//                    Thread.sleep(1000);
-//                    addback(getContent("https://vuzopedia.ru/vuz/1751/programs/bakispec/186"));
-//                    addback(getContent("https://vuzopedia.ru/vuz/1751/programs/bakispec/6122"));
-//                    addback(getContent("https://vuzopedia.ru/vuz/2554/programs/bakispec/125"));
-//                } catch (IOException e) {
-//                    throw new RuntimeException(e);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//
-//            }
-//        }).start();
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+                @Override
+                public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                    super.onOpen(db);
+                }
+            };
+            universityDatabase = Room.databaseBuilder(getApplicationContext(), UniversityDatabase.class,
+                    "University").addCallback(myCallback).build();
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        University MGU1 = new University("МГУ", "Системное программирование и компьютерные науки", 405, 300, "Информатика, Математика (профиль), Русский язык, Физика", "Платное/Бесплатное", "Москва", 409610,"https://vuzopedia.ru/vuz/1/programs/bakispec/62",  4, "Математика");
+                        University MGU2 = new University("МГУ", "Прикладная математика и физика", 275, 126, "Математика (профиль), Русский язык, Физика", "Платное/Бесплатное", "Москва", 435970, "https://vuzopedia.ru/vuz/1/napr/93", 4, "Физика");
+                        University MGTU1 = new University("МГТУ", "Бизнес-информатика", 278, 241, "Математика (профиль), Русский язык, Обществознание", "Платное/Бесплатное" , "Москва",324143 , "https://vuzopedia.ru/vuz/4/programs/bakispec/603", 3, "Без вступительных испытаний");
+                        University MGTU2 = new University("МГТУ", "Бизнес-информатика", 278, 241, "Информатика, Математика (профиль), Русский язык", "Платное/Бесплатное" , "Москва",324143 , "https://vuzopedia.ru/vuz/4/programs/bakispec/603", 3, "Без вступительных испытаний");
+
+
+                        University MFTI1 = new University("МФТИ", "Системный анализ и управление", 295, 280, "Информатика, Математика (профиль), Русский язык", "Платное/Бесплатное", "Москва", 504000, "https://vuzopedia.ru/vuz/591/programs/bakispec/130",  3, "Без вступительных испытаний");
+                        University MFTI2 = new University("МФТИ", "Системный анализ и управление", 295, 280, "Математика (профиль), Русский язык, Физика", "Платное/Бесплатное", "Москва", 504000, "https://vuzopedia.ru/vuz/591/programs/bakispec/130",  3, "Без вступительных испытаний");
+
+                        University NGU1 = new University("НГУ", "Организационная психология", 249, 177, "Биология, Математика (профиль), Русский язык", "Платное/Бесплатное" ,"Новосибирск", 125000, "https://vuzopedia.ru/vuz/1612/programs/bakispec/561", 4, "Без вступительных испытаний");
+
+
+                        addback(MGU1);
+                        addback(MGU2);
+                        addback(MGTU1);
+                        addback(MGTU2);
+
+                        addback(MFTI1);
+                        addback(MFTI2);
+                        addback(NGU1);
+
+
+
+                        addback(getContent("https://vuzopedia.ru/vuz/1848/programs/bakispec/87"));
+                        addback(getContent("https://vuzopedia.ru/vuz/725/programs/bakispec/164"));
+                        addback(getContent("https://vuzopedia.ru/vuz/1189/programs/bakispec/2272"));
+                        Thread.sleep(1000);
+                        addback(getContent("https://vuzopedia.ru/vuz/1567/programs/bakispec/1666"));
+                        addback(getContent("https://vuzopedia.ru/vuz/342/programs/bakispec/108"));
+                        addback(getContent("https://vuzopedia.ru/vuz/342/programs/bakispec/2277"));
+                        Thread.sleep(1000);
+                        addback(getContent("https://vuzopedia.ru/vuz/1189/programs/bakispec/46"));
+                        addback(getContent("https://vuzopedia.ru/vuz/725/programs/bakispec/1546"));
+                        addback(getContent("https://vuzopedia.ru/vuz/342/programs/bakispec/81"));
+                        Thread.sleep(1000);
+                        addback(getContent("https://vuzopedia.ru/vuz/1751/programs/bakispec/186"));
+                        addback(getContent("https://vuzopedia.ru/vuz/1751/programs/bakispec/6122"));
+                        addback(getContent("https://vuzopedia.ru/vuz/2554/programs/bakispec/125"));
+                        addback(getContent("https://vuzopedia.ru/vuz/1751/programs/bakispec/85"));
+                        addback(getContent("https://vuzopedia.ru/vuz/1612/programs/bakispec/400"));
+
+                        //Thread.sleep(500);
+
+
+                    } catch (IOException ex) {
+                        content = null;
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }).start();
+        }
+
+
 
     }
 
@@ -179,7 +176,7 @@ Handler handler = new Handler(Looper.getMainLooper());
                     }, time_streams);
                 }
                 else {
-                    setProgressValue(progress + 1);
+                    setProgressValue(progress + step);
                 }
 
                 if(progress==150)
