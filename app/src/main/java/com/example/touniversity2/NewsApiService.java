23a7cab6304b3fddd5270c8cbf349b8d4b0c89ce
alexander <149459305+alexander92322@users.noreply.github.com>
2024-07-04 10:43:51 +0700

    Update NewsApiService.java

--- app/src/main/java/com/example/touniversity2/NewsApiService.java
+++ app/src/main/java/com/example/touniversity2/NewsApiService.java
@@ -4,6 +4,6 @@ import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.GET;
 
 public interface NewsApiService {
-    @GET("everything?sortBy=publishedAt&apiKey=9f6e410679994ec1a9f0a8135d5475f5&q=ВУЗы&q=Россия&q=ВШЭ")
+    @GET("everything?sortBy=publishedAt&q=ВУЗы&q=Россия&q=ВШЭ")
     Observable<NewsResponse> getNews();
 }
